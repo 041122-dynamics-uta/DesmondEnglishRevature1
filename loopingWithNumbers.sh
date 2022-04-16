4938a1725d59e58e536b302ab72e65e4e90bcb61
Desmond English <desmond389@revature.net>
2022-04-16 13:40:21 -0500

    Looping With Numbers

---
+++ loopingWithNumbers.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+#Looping with Numbers
+
+Use a *for* loop to display the natural numbers from 1 to 50.
+
+##############################################
+
+#Use a for loop to countdown from 1 to 50.
+
+#input
+for i in {1..50}
+do 
+    echo $i
+done
