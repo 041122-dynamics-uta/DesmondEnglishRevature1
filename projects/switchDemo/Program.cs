b7ee9f5c8f8ca2eedb60b06425f0fee3ff5c858a
Desmond English <desmond389@revature.net>
2022-04-24 14:06:01 -0500

    Update

--- projects/switchDemo/Program.cs
+++ projects/switchDemo/Program.cs
@@ -5,35 +5,37 @@ namespace switchDemo
     class Program
     {
         static void Main(string[] args)
-     {
+        {
             /* The default keyword is optional and specifies some code to run 
                 if there is no case match: */
 
-        Console.WriteLine("Who is the best AVENGER?");
+            Console.WriteLine("Who is the best AVENGER?");
 
-        int avenger = 3;
-        switch (avenger) 
+            Console.ReadLine();
+
+            int avenger = 3;
+            switch (avenger)
             {
-            case 4:
-                Console.WriteLine("Black Panther is the best AVENGER!");
-                break;
-            case 5:
-                Console.WriteLine("Spider-Man is the best AVENGER!");
-                break;
-            case 6:
-                Console.WriteLine("Doctor Strange is the best AVENGER!");
-                break;
-            case 7:
-                Console.WriteLine("Vision is the best AVENGER!");
-                break;
-            case 8:
-                Console.WriteLine("Moon Knight is the best AVENGER!");
-                break;   
-            default:
-                Console.WriteLine("Wanda is the only answer for best AVENGER!");
-                break;
+                case 4:
+                    Console.WriteLine("Black Panther is the best AVENGER!");
+                    break;
+                case 5:
+                    Console.WriteLine("Spider-Man is the best AVENGER!");
+                    break;
+                case 6:
+                    Console.WriteLine("Doctor Strange is the best AVENGER!");
+                    break;
+                case 7:
+                    Console.WriteLine("Vision is the best AVENGER!");
+                    break;
+                case 8:
+                    Console.WriteLine("Moon Knight is the best AVENGER!");
+                    break;
+                default:
+                    Console.WriteLine("Wanda is the only answer for best AVENGER!");
+                    break;
             }
-                // Outputs "Wanda is the only answer for best AVENGER!"
+            // Outputs "Wanda is the only answer for best AVENGER!"
         }
     }
 }
