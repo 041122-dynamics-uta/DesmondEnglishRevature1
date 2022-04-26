2d410bbf897346952c40ea6f9dd2ed1016191056
Desmond English <desmond389@revature.net>
2022-04-26 09:49:20 -0500

    Update

---
+++ projects/oopdemo/Human.cs
@@ -0,0 +1,17 @@
+namespace oopdemo
+{
+    public class Human
+    {
+        // Private is an access modifier
+        public string fname = "Jim";
+        public string lname = "Harrow";
+
+
+
+
+
+
+
+
+    }
+}
